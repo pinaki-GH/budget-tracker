1b655a0ab680f29fd693d7367d97561d9cfb472b
pinaki-GH <pinaki4frndz@gmail.com>
2026-06-10 11:51:21 +0530

    Update page.js

--- app/add-expense/page.js
+++ app/add-expense/page.js
@@ -234,6 +234,10 @@ const availableMonths =
           <button>Forex Rates</button>
         </Link>
 
+        <Link href="/resource-master" style={{ marginLeft: 10 }}>
+          <button>Resource Master</button>
+        </Link>
+
         <Link href="/projections" style={{ marginLeft: 10 }}>
           <button>Projections</button>
         </Link>

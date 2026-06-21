3f581c1c0c236f35907e9057f9e82fb07006a5a0
pinaki-GH <pinaki4frndz@gmail.com>
2026-06-22 00:30:56 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -978,6 +978,15 @@ const yearlyUtilization =
       <strong>
         Carry Forward
       </strong>
+      <div
+  style={{
+    fontSize: 12,
+    color: '#666',
+    marginBottom: 15
+  }}
+>
+  From previous quarter within the year
+</div>
       <br />
       kr {quarterCarryForward.toFixed(2)}
     </p>

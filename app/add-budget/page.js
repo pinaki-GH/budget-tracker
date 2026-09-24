759df3400f685a740882fa2744f15a28e47facaf
pinaki-GH <pinaki4frndz@gmail.com>
2026-09-24 23:25:00 +0530

    Update page.js

--- app/add-budget/page.js
+++ app/add-budget/page.js
@@ -204,6 +204,10 @@ export default function AddBudget() {
           <button>Resource Master</button>
         </Link>
 
+        <Link href="/leave-data" style={{ marginLeft: 10 }}>
+          <button>Leave Data</button>
+        </Link>
+        
         <Link href="/projections" style={{ marginLeft: 10 }}>
           <button>Projection Planning</button>
         </Link>
